dd44219eff4b1463beaa7cc7a4c078772473e821
Calle <36276161+qalle-fivem@users.noreply.github.com>
2019-03-28 20:50:32 +0100

    Initial

---
+++ client/main.lua
@@ -0,0 +1,18 @@
+
+Citizen.CreateThread(function()
+	Citizen.Wait(100)
+
+	while true do
+		local sleepThread = 500
+
+		local radarEnabled = IsRadarEnabled()
+
+		if not IsPedInAnyVehicle(PlayerPedId()) and radarEnabled then
+			DisplayRadar(false)
+		elseif IsPedInAnyVehicle(PlayerPedId()) and not radarEnabled then
+			DisplayRadar(true)
+		end
+
+		Citizen.Wait(sleepThread)
+	end
+end)
