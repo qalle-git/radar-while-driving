030f56b1c9e26cce94fed7a2f14805d05de2cd46
Kawan <43068452+Kawan@users.noreply.github.com>
2021-09-05 16:01:57 +0200

    Update main.lua
    Minimap is flashing because of the sleep thread. This will fix the issue.

--- client/main.lua
+++ client/main.lua
@@ -1,17 +1,11 @@
 Citizen.CreateThread(function()
-	Citizen.Wait(100)
-
 	while true do
-		local sleepThread = 500
-
-		local radarEnabled = IsRadarEnabled()
-
 		if not IsPedInAnyVehicle(PlayerPedId()) and radarEnabled then
 			DisplayRadar(false)
 		elseif IsPedInAnyVehicle(PlayerPedId()) and not radarEnabled then
 			DisplayRadar(true)
 		end
 
-		Citizen.Wait(sleepThread)
+		Citizen.Wait(1)
 	end
 end)
